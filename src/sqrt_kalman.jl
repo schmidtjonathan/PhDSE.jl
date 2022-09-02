b197c605db50690f7d23817c06c59ca5cc3eca83
Jonathan Schmidt <jonathan.schmidt@uni-tuebingen.de>
2022-09-02 14:26:11 +0200

    :memo: Docstrings

--- src/sqrt_kalman.jl
+++ src/sqrt_kalman.jl
@@ -2,12 +2,18 @@
     sqrt_kf_predict!(fcache, Φ, Q, [u])
 
 Efficient and in-place implementation of the prediction step in a square-root Kalman filter.
+Computes the same posterior distribution as the standard Kalman filter ([`kf_predict!`](@ref))
+but is numerically more stable.
+Works entirely on matrix-square-roots of the covariance matrices.
 
 # Arguments
 - `fcache::KFCache`: a cache holding memory-heavy objects
 - `Φ::AbstractMatrix`: transition matrix, i.e. dynamics of the state space model
 - `Q::PSDMatrix`: **right** matrix square root of transition covariance, i.e. process noise of the state space model
 - `u::AbstractVector` (optional): affine control input to the dynamics
+
+# References
+[1] Krämer, N., & Hennig, P. (2020). Stable implementation of probabilistic ODE solvers.
 """
 function sqrt_kf_predict!(
     fcache::SqrtKFCache,
@@ -33,6 +39,9 @@ end
     sqrt_kf_correct!(fcache, H, R, y, [v])
 
 Efficient and in-place implementation of the correction step in a square-root Kalman filter.
+Computes the same posterior distribution as the standard Kalman filter ([`kf_correct!`](@ref))
+but is numerically more stable.
+Works entirely on matrix-square-roots of the covariance matrices.
 
 # Arguments
 - `fcache::KFCache`: a cache holding memory-heavy objects
@@ -40,6 +49,10 @@ Efficient and in-place implementation of the correction step in a square-root Ka
 - `H::AbstractMatrix`: measurement matrix of the state space model
 - `R::PSDMatrix`: **right** matrix square root of measurement noise covariance of the state space model
 - `v::AbstractVector` (optional): affine control input to the measurement
+
+# References
+[1]:
+Krämer, N., Bosch, N., Schmidt, J. & Hennig, P. (2022). Probabilistic ODE Solutions in Millions of Dimensions.
 """
 function sqrt_kf_correct!(
     fcache::SqrtKFCache,
