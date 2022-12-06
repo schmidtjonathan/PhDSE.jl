0deb8d26028b52e70451b1b0d1057eccdbd3a762
Jonathan Schmidt <jonathan.schmidt@uni-tuebingen.de>
2022-12-06 17:35:34 +0100

    EnKF: Implement OOP versions for testing

--- src/ensemble_kalman.jl
+++ src/ensemble_kalman.jl
@@ -1,3 +1,109 @@
+ensemble_mean(ens::AbstractMatrix) = sum(ens, dims=2) / size(ens, 2)
+centered_ensemble(ens::AbstractMatrix) = ens .- ensemble_mean(ens)
+
+function enkf_predict(
+    ensemble::AbstractMatrix{T}
+    Φ::AbstractMatrix{T},
+    process_noise_dist::MvNormal,
+    u::Union{AbstractVector{T},Missing} = missing,
+) where {T}
+    N = size(ensemble, 2)
+    forecast_ensemble = Φ * ensemble + Distributions.rand!(process_noise_dist, N)
+    if !ismissing(u)
+        forecast_ensemble .+= u
+    end
+
+    # Compute sample mean of forecast ensemble
+    # todo?
+    # Calculate zero-mean forecast ensemble by subtracting the mean
+    # todo?
+
+    return forecast_ensemble
+end
+
+
+function enkf_correct(
+    forecast_ensemble::AbstractMatrix{T},
+    H::AbstractMatrix{T},
+    measurement_noise_dist::MvNormal,
+    y::AbstractVector{T},
+    v::Union{AbstractVector{T},Missing} = missing,
+) where {T}
+    N = size(forecast_ensemble, 2)
+    HX = H * forecast_ensemble
+    if !ismissing(v)
+        HX .+= v
+    end
+    data_plus_noise = rand(measurement_noise_dist, N) .+ y
+    residual = data_plus_noise - HX
+
+    A = centered_ensemble(forecast_ensemble)
+    C = Symmetric(A * A') / (N - 1)
+    cross_covariance = C * H'
+    Ŝ = Symmetric(H * cross_covariance + measurement_noise_dist.Σ, :L)
+    K̂ = cross_covariance / Ŝ
+    ensemble = forecast_ensemble + K̂ * residual
+    return ensemble
+end
+
+
+function enkf_matrixfree_correct(
+    forecast_ensemble::AbstractMatrix{T},
+    HX::AbstractMatrix{T},
+    HA::AbstractMatrix{T},
+    measurement_noise_dist::MvNormal,
+    y::AbstractVector{T},
+    R_inverse::Union{AbstractMatrix{T},Missing} = missing,
+    v::Union{AbstractVector{T},Missing} = missing,
+) where {T}
+    D, N = size(forecast_ensemble)
+    Nsub1 = N - 1
+    d = length(y)
+    data_plus_noise = rand(measurement_noise_dist, N) .+ y
+    residual = data_plus_noise - HX
+    A = centered_ensemble(forecast_ensemble)
+
+    compute_P_inverse = (d > N) && !ismissing(R_inverse)
+    if compute_P_inverse
+        # Implementation from the paper/preprint by Mandel; Section 4.2
+        # uses the Matrix inversion lemma to be optimal for d >> N
+        # -------------------------------------------------------------
+        # R⁻¹ (D - HX)
+        T1 = R_inverse * residual
+        # (HA)' R⁻¹(D - HX)
+        T2 = HA' * T1
+        # Q := I_N + (HA)'(R⁻¹/ (N-1)) (HA)
+        #  -> R⁻¹(HA) / (N-1)
+        QT1 = (R_inverse / Nsub1) * HA
+        #  -> (HA)'R⁻¹(HA) / (N-1)
+        QT2 = HA' * QT1
+        Q = Symmetric(I(D) + QT2, :L)
+        # Q⁻¹ (HA)' R⁻¹(D - HX)
+        T3 = Q \ T2
+        # (1/N-1) * (HA) Q⁻¹ (HA)' R⁻¹(D - HX)
+        T4 = Nsub1 \ (HA * T3)
+        # (D - HX) - T4 (results from the identity matrix in [I - ...]; see Sec. 4.2
+        T5 = residual - T4
+        # P⁻¹(D - HX) = R⁻¹((D - HX) - ((1/N-1) * (HA) Q⁻¹ (HA)' R⁻¹(D - HX)))
+
+        P_inv_times_res = R_inverse * T5  # this is [d x d] * [d x N] -> [d x N]
+    else
+        # If N > d, use the standard approach instead, computing P,
+        # instead of P⁻¹
+        P = Nsub1 \ Symmetric(HA * HA' + measurement_noise_dist.Σ, :L)
+
+        P_inv_times_res = P \ residual  # this is [d x d] * [d x N] -> [d x N]
+    end
+    # Now we insert P⁻¹(D - HX) into Eq. (2.2) ; see Sec. 2
+    # (HA)' P⁻¹(D - HX)
+    NxN_term = HA' * P_inv_times_res  # this is [N x d] * [d x N] -> [N x N]
+    DxN_term = Nsub1 \ A * NxN_term  # this is [D x N] * [N x N] -> [N x N]
+
+    ensemble = forecast_ensemble + DxN_term
+    return ensemble
+end
+
+
 """
     enkf_predict!(fcache, Φ, Q, [u])
 
@@ -108,9 +214,9 @@ function omf_enkf_correct!(
     # (HA)' R⁻¹(D - HX)
     mul!(fcache.NxN_cache01, fcache.HA', fcache.dxN_cache02)
 
-    # Q := I_N + (HA)'R⁻¹(HA) / (N-1)
+    # Q := I_N + (HA)'(R⁻¹/ (N-1)) (HA)
     #  -> R⁻¹(HA) / (N-1)
-    rdiv!(mul!(fcache.dxN_cache03, R_inv, fcache.HA), Nsub1)
+    mul!(fcache.dxN_cache03, rdiv!(R_inv, Nsub1), fcache.HA)
     #  -> (HA)'R⁻¹(HA) / (N-1)
     mul!(fcache.NxN_cache02, fcache.HA', fcache.dxN_cache03) # That's Q without the added Identity matrix
     #  -> I_N + (HA)'R⁻¹(HA) / (N-1)
@@ -122,8 +228,8 @@ function omf_enkf_correct!(
     #                        |   by cholesky!   | |     by ldiv!     |
     ldiv!(cholesky!(Symmetric(fcache.NxN_cache02)), fcache.NxN_cache01)
 
-    # K := (HA) Q⁻¹ (HA)' R⁻¹(D - HX)
-    rdiv!(mul!(fcache.dxN_cache02, fcache.HA, fcache.NxN_cache01), Nsub1)
+    # K := (1/N-1) * (HA) Q⁻¹ (HA)' R⁻¹(D - HX)
+    ldiv!(Nsub1, mul!(fcache.dxN_cache02, fcache.HA, fcache.NxN_cache01))
 
     # (D - HX) - K
     fcache.dxN_cache01 .-= fcache.dxN_cache02
@@ -136,7 +242,7 @@ function omf_enkf_correct!(
 
     # Xᵃ = Xᶠ + A / (N-1) (HA)' R⁻¹((D - HX) - K)
     copy!(fcache.ensemble, fcache.forecast_ensemble)
-    mul!(fcache.ensemble, rdiv!(fcache.A, Nsub1), fcache.NxN_cache02, 1.0, 1.0)
+    mul!(fcache.ensemble, ldiv!(Nsub1, fcache.A), fcache.NxN_cache02, 1.0, 1.0)
 end
 
 export enkf_predict!
