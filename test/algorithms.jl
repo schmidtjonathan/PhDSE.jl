2a778bba061367f8f6bd560d66e790f66372a8dd
Jonathan Schmidt <jonathan.schmidt@uni-tuebingen.de>
2022-12-19 08:06:41 +0100

    Fix rebase artifact

--- test/algorithms.jl
+++ test/algorithms.jl
@@ -153,68 +153,6 @@ function filtering_setup(D=100, num_obs=100)
         )
     end
 
-    d = size(drift, 1)
-    M = [drift dispersion*dispersion'; zero(drift) -drift']
-    Mexp = exp(dt * M)
-    A_breve = Mexp[1:d, 1:d]
-    Q_breve = Mexp[1:d, d+1:end] * A_breve'
-
-    A = kron(I(wiener_process_dimension), A_breve)
-    Q = kron(I(wiener_process_dimension), Q_breve)
-    @assert Q ≈ Q'
-    sQ = Symmetric(0.5 * (Q + Q'))
-    return A, sQ
-end
-
-function simulate_linear(
-    A, Q, u, H, R, v, μ₀, Σ₀, N::Int,
-)
-    x = rand(Xoshiro(1), MvNormal(μ₀, Σ₀))
-    states = [x]
-    observations = []
-
-    for i in 1:N
-        push!(states, rand(Xoshiro(12), MvNormal(A * states[end] .+ u, Q)))
-        push!(observations, rand(Xoshiro(34), MvNormal(H * states[end] .+ v, R)))
-    end
-    return states, observations
-end
-
-stack(x) = copy(reduce(hcat, x)')
-
-function filtering_setup(D = 100, observe_every = 3, num_obs = 30)
-    Random.seed!(1234)
-
-    σ₀ = 0.001
-    σᵣ = 0.03
-
-    matern_derivs = 1
-    totaldim = D * (matern_derivs + 1)
-
-    μ₀ = rand(totaldim)
-    Σ₀ = diagm(0 => σ₀ .* ones(totaldim))
-
-    tspan = (0.0, 1.0)
-    dt = (tspan[2] - tspan[1]) / num_obs
-    A, Q = _matern(D, matern_derivs, 1.0, dt)
-
-    H = diagm(0 => ones(D))[1:observe_every:end, :] * projectionmatrix(D, matern_derivs, 0)
-    measdim, totaldim = size(H)
-
-    R = diagm(0 => σᵣ .* ones(measdim))
-
-    u = 0.1 .* randn(totaldim)
-    v = zeros(measdim)
-
-    ground_truth, observations = simulate_linear(A, Q, u, H, R, v, μ₀, Σ₀, num_obs)
-
-    if PLOT_RESULTS
-        savefig(
-            plot_test(stack(ground_truth), stack(observations), H),
-            joinpath(mkpath("./out/"), "setup.png"),
-        )
-    end
-
     return μ₀, Σ₀, A, Q, u, H, R, v, ground_truth, observations
 end
 
